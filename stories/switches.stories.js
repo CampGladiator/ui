1036b7927243e99c6c3e0195ad6e34a3b1025250
renanpvaz <renan.piresvz@gmail.com>
2019-03-21 16:52:17 -0300

    [CGUI-112] Add switch component

---
+++ stories/switches.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { storiesOf, addDecorator } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import '../build/main.css'
+import { StoryLayout } from './util'
+
+storiesOf('Switches', module)
+  .add('Default', () => (
+    <StoryLayout rows={2} gap="50px">
+      <span className="switch" role="button" aria-pressed="false" />
+      <span className="switch switch--on" role="button" aria-pressed="true" />
+    </StoryLayout>
+  ))
+  .add('Interactive', () => (
+    <StoryLayout>
+      <span
+        className="switch"
+        onClick={e => e.target.classList.toggle('switch--on')}
+      />
+    </StoryLayout>
+  ))
