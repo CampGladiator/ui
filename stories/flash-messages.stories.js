2230b9766501f47286abfb784e945685734183a2
renanpvaz <renan.piresvz@gmail.com>
2019-03-22 15:50:53 -0300

    [CGUI-112] Add viewport addon and mobile flash banner

--- stories/flash-messages.stories.js
+++ stories/flash-messages.stories.js
@@ -7,15 +7,34 @@ import { linkTo } from '@storybook/addon-links'
 import '../build/main.css'
 import { StoryLayout } from './util'
 
-storiesOf('Flash Messages', module).add('Default', () => (
-  <StoryLayout template="80vw">
-    <div className="flash-message">
-      <span className="flash-message__badge">0</span>
-      <section className="flash-message__content">
-        <p>Link alert message goes here</p>
-        <button className="button button--outline button--white">action</button>
-      </section>
-      <span>close</span>
-    </div>
-  </StoryLayout>
-))
+storiesOf('Flash Messages', module)
+  .add('Default', () => (
+    <StoryLayout template="80vw">
+      <div className="flash-message">
+        <span className="flash-message__badge">0</span>
+        <section className="flash-message__content">
+          <p>Link alert message goes here</p>
+          <button className="button button--outline button--white">
+            action
+          </button>
+        </section>
+        <span>close</span>
+      </div>
+    </StoryLayout>
+  ))
+  .add(
+    'Mobile',
+    () => (
+      <div className="flash-message">
+        <span className="flash-message__badge">0</span>
+        <section className="flash-message__content">
+          <p>Link alert message goes here</p>
+          <button className="button button--outline button--white">
+            action
+          </button>
+        </section>
+        <span>&times;</span>
+      </div>
+    ),
+    { viewport: { defaultViewport: 'iphone5' } },
+  )
