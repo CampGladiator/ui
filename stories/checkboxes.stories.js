859afb901682c92ca783d332de32f64a11d2ea13
renanpvaz <renan.piresvz@gmail.com>
2019-03-29 09:58:38 -0300

    [CGUI-114] Remove assets and move 'em to cdn

--- stories/checkboxes.stories.js
+++ stories/checkboxes.stories.js
@@ -6,8 +6,14 @@ storiesOf('Checkboxes', module)
   .addParameters({ layout: { rows: 2, justify: 'center', gap: '20px' } })
   .add('Default', () => (
     <>
-      <input className="checkbox" type="checkbox" />
-      <input className="checkbox" checked type="checkbox" />
+      <div className="checkbox">
+        <input className="checkbox__input" type="checkbox" />
+        <i className="checkbox__tick" />
+      </div>
+      <div className="checkbox">
+        <input className="checkbox__input" checked type="checkbox" />
+        <i className="checkbox__tick" />
+      </div>
     </>
   ))
   .add('Labeled', () => (
@@ -17,12 +23,14 @@ storiesOf('Checkboxes', module)
         <label className="checkbox__label" htmlFor="1">
           Checkbox label
         </label>
+        <i className="checkbox__tick" />
       </div>
       <div className="checkbox">
         <label className="checkbox__label" htmlFor="2">
           Checkbox label
         </label>
         <input id="2" className="checkbox__input" type="checkbox" />
+        <i className="checkbox__tick" />
       </div>
     </>
   ))
